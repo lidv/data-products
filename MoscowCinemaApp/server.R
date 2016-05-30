75a6877589d6e800db92517d518863883142e793
dlishmanov <dlishmanov@guidewire.com>
2016-05-30 19:44:14 +0300

    Almost final version of Shiny app

---
+++ MoscowCinemaApp/server.R
@@ -0,0 +1,73 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(jsonlite)
+library(rCharts)
+
+films <- fromJSON("../data/films.json")[["Cells"]]
+films$Filmino_en[lapply(films$Filmino_en,length)==0] = "undefined"
+
+countries <<- sort(unique(unlist(films$ProducingCountry_en)))
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  dt <- reactive({
+    films$ProducingCountry_en <- lapply(films$ProducingCountry_en,paste)
+    minyear <- input$years[1]
+    maxyear <- input$years[2]
+    fCountries <- input$countries
+    
+    # Apply filters
+    dt <- films %>%
+      filter(
+        Year_en >= minyear,
+        Year_en <= maxyear,
+        ProducingCountry_en %in% fCountries
+      )
+    dt
+  })
+  
+  output$countries <- renderUI({
+    if(1) {
+      checkboxGroupInput('countries', 'Producing Country', countries, selected=countries)
+    }
+  })
+  
+  output$studio <- renderUI({
+    if(1) {
+      selectInput("studio","Producing Studio",studios,selected = "All")
+    }
+  })
+
+  output$filmsByYears <- renderChart({
+    df <- dt()
+    df <- aggregate(df$global_id, by=list(df$Year_en, df$FilmType_en), FUN = length)
+    n1 <- nPlot(x ~ Group.1, group = "Group.2", data = df, type = 'multiBarChart', dom = 'filmsByYears', width = 650)
+  })
+
+  output$filmsByIno <- renderChart({
+    df <- dt()
+    df$FilmStudio_en <- unlist(df$FilmStudio_en)
+    df$Filmino_en <- unlist(df$Filmino_en)
+    df <- aggregate(df$global_id, by=list(df$Filmino_en, df$AgeRestrictions_en), FUN = length)
+    n1 <- nPlot(x ~ Group.1, group = "Group.2", data = df, type = 'multiBarHorizontalChart', dom = 'filmsByIno', width = 650)
+  })
+  
+  output$data <- renderDataTable ({
+    films$ProducingCountry_en <- lapply(films$ProducingCountry_en,paste)
+    films$Duration_en <- lapply(films$Duration_en,paste)
+    films$FilmStudio_en <- lapply(films$FilmStudio_en,paste)
+    films$Filmino_en <- lapply(films$Filmino_en,paste)
+    films
+  }, options = list(searching = FALSE, pageLength = 50))
+  
+  
+})
---
+++ MoscowCinemaApp/ui.R
@@ -0,0 +1,23 @@
+library(shiny)
+library(ggvis)
+
+
+shinyUI(
+  navbarPage("Film fund \"Moscow cinema\" explorer" ,
+    tabPanel("Visual",
+      sidebarPanel(
+        sliderInput("years", "Year released", 1938, 2008, value = c(1938, 2008),sep=""),
+        uiOutput("countries"),
+        uiOutput("studio")
+      ),
+      mainPanel(
+        h4('Films by year of production', align = "center"),
+                 showOutput("filmsByYears", "nvd3"),
+        h4('Films by genre', align = "center"),
+        showOutput("filmsByIno", "nvd3")
+      )
+    ),
+    tabPanel('Data',mainPanel(dataTableOutput(outputId="data"),downloadButton('downloadData', 'Download'))),
+    tabPanel("About",mainPanel(includeMarkdown("../README.Rmd")))
+  )
+)
